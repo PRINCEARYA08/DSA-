a97bd9dbb9d27b8c2ccf25ec96d24783ddebd82b
PRINCE ARYA <Parya3252@gmail.com>
2026-04-02 22:35:55 +0530

    6.cpp
    Implement a program that prints the number 78 when iterating from 1 to 100.

---
+++ practic/6.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n = 100;
+    // cin>>n;
+
+    for (int i = 1; i <= n; i++)
+    {
+       if (i==78)
+       {
+        cout << i;
+       }
+       else{
+
+       }
+        // cout << i;
+        // cout << endl;
+    }
+    return 0;
+}
