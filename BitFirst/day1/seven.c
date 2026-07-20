4e7ae7d5ff2de22cc49a03d944483daee5655cb4
PRINCE ARYA <Parya3252@gmail.com>
2026-07-20 19:37:24 +0530

    seven.modife

--- BitFirst/day1/seven.c
+++ BitFirst/day1/seven.c
@@ -1,5 +1,30 @@
 #include<stdio.h>
 void main(){
-   int x= printf("Hello");
-printf("%d",x);
+
+
+//    int x= printf("firstbit");
+// printf("%d",x);
+
+
+// int x = printf("firstbit ")+ printf("solution ");
+// printf("%d",x);
+
+
+// int x = printf("firstbit ")> printf("solution ");
+// printf("%d",x);
+
+
+// int x = printf("firstbit ") || printf("solution ");
+// printf("%d",x);
+
+// int x = printf("firstbit ")&& printf("solution ");
+// printf("%d",x);
+
+// int x =printf("hello %d",printf("fbs"));
+// printf("%d",x);
+
+// int x= printf("ab")+printf("pqr")>10+2 && printf("xyz")-printf("imnop")|| printf("fbs");
+// printf("%d",x);
+
+
 }
