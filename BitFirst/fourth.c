589680e9cbbcbb053329b4a53fa1bfd72fcf1557
PRINCE ARYA <Parya3252@gmail.com>
2026-07-20 19:00:21 +0530

    furth

---
+++ BitFirst/fourth.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main(){
+    int a,b,c;
+    a=12;
+    b=15;
+    
+    int sum=a+b;
+    //printf("Addition of %d and %d is %d",a,b,sum);
+    printf("Addition of %d and %d is %d",a,b);
+}
