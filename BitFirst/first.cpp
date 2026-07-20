2aec5201938ad992a6fdf5b27a6f504d53f6ef98
PRINCE ARYA <Parya3252@gmail.com>
2026-07-20 16:35:41 +0530

    three

---
+++ BitFirst/first.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int addNumber(int a, int b){
+    int sum = a+b;
+    return sum;
+}
+int main(){
+     int result;
+    result = addNumber(7,3);
+   // cout<<result;
+   // cout<<"Hello";
+   10;
+    return 0;
+}
