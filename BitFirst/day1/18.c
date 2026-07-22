29fbb3d9913e15b1fa62384d38bd97e6f1a2a475
PRINCE ARYA <Parya3252@gmail.com>
2026-07-22 15:00:39 +0530

    19.cpp

--- BitFirst/day1/18.c
+++ BitFirst/day1/18.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
 void main(){
-    int a=9;
-    int x = printf("%d",a++);
-    printf("a=%d x=%d",a,x);
+    // int a=9;
+    // int x = printf("%d",a++);
+    // printf("a=%d x=%d",a,x);
+
+
+    int num,r1,r2,q1,sum;
+    num=121;
+    
 }
---
+++ DSA 2026/day1/19.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+   // int n=5;
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+           cout<<"a";
+        }
+        
+        cout<<endl;
+    }
+    
+    return 0;
+}
