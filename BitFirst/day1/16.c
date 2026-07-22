ff8bd03290fb91b356d4ac50ca287d9edd1fede6
PRINCE ARYA <Parya3252@gmail.com>
2026-07-22 13:58:29 +0530

    15.c

--- BitFirst/day1/16.c
+++ BitFirst/day1/16.c
@@ -5,8 +5,18 @@ void main(){
     // printf("%d",a);
 
 
-     int a=10,b;
-    b=a++;
-    printf("%d %d",a,b);
+    //  int a=10,b;
+    // b=a++;
+    // printf("%d %d",a,b);
+
+// int a=10,b=20,c=30,d=40;
+
+// int x=a++ +  b++ + --c + d--;
+// printf("%d %d %d %d %d ",x,a,b,c,d);
+
+int a=10,b=20;
+int x = a++ + ++a + b++;
+printf("%d ",x);
+
 
 }
