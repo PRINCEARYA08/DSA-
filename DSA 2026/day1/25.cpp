0786fa6bd19197e03ad5220885ab2e5f833b7fce
PRINCE ARYA <Parya3252@gmail.com>
2026-07-24 17:16:30 +0530

    25.cpp

---
+++ DSA 2026/day1/25.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"Enter the value of n: ";
+    cin>>num;
+    
+    for (int i = 1; i <= num; i++)
+    {
+        if (i%2==!0)
+        {
+            cout<<i<<endl;
+        }
+        
+    }
+
+    return 0;
+}
