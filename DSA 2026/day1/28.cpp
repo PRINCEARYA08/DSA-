543ac2bdd766f0cd1af8567dbe0223565da58c6f
PRINCE ARYA <Parya3252@gmail.com>
2026-07-24 17:30:08 +0530

    28.cpp

---
+++ DSA 2026/day1/28.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=10;
+    for (int  i = n; i >= 0; i--)
+    {
+        if (i%2==0)
+        {
+              cout<<i<<endl;
+        }
+        
+     
+    }
+    
+    return 0;
+}
