50e1f1a4ce94e9aa6d90fefcf4b5831688447c2c
PRINCE ARYA <Parya3252@gmail.com>
2026-07-29 15:27:22 +0530

    22.c

---
+++ BitFirst/day1/22.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+void main()
+{
+    int num = -4;
+    if (num > 3)
+    {
+
+        if (num > 8)
+        {
+            printf("number is greater then 8");
+        }
+        else
+        {
+            printf("number greater 3");
+        }
+    }
+    else if (num < 3)
+    {
+
+        if (num < 0)
+        {
+            printf("number is nevative");
+        }
+        else
+        {
+            printf("number less then 3");
+        }
+    }
+    else
+    {
+    }
+}
+
+//find the number greater amoung 3 check nois +ve , -ve, 0
